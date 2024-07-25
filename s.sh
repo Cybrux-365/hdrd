c1cd1dc3b84644bf33901f740d53967a43e0161c
Cybrux-365 <laro1laro34@gmail.com>
2024-07-25 15:10:28 +0100

    Test

---
+++ s.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+y=$(gcc wr.c)
+q=$(./a.out)
+x=$(chmod 444 file)
+z=$(gcc lib.c rd.c)
+a=$(./a.out)
--- wr.c
+++ wr.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <fcntl.h>
 int main (void)
 {
     int fd;
